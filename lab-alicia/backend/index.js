4ca9cb3b4d3d7634cdd64a642a49e8ca6635b401
alicialycan <alicialycan@gmail.com>
2018-06-06 19:12:49 -0700

    receiving and sending click event data!

--- lab-alicia/backend/index.js
+++ lab-alicia/backend/index.js
@@ -1,28 +1,32 @@
 const express = require('express');
 const app = express();
-// const http = require('http').Server(app);
-// const io = require('socket.io')(http);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-// let clicks = 0;
+let clicks = 0;
 
-// io.on('connection', socket => {
-//   console.log('connected', socket.id);
-//   console.log('sending initial click info', clicks);
-//   io.emit('click-info', {clicks});
+io.on('connection', socket => {
+  console.log('connected', socket.id);
+  console.log('sending initial click info', clicks);
+  io.emit('click-info', {clicks});
 
-//   socket.on('increment click', () => {
-//     console.log('got click');
-//     clicks++;
-//     console.log('broadcasting click', clicks);
-//     io.emit('click-info', {clicks});
-//   });
-// });
+  socket.on('send-click', () => {
+    console.log('got "send-click event"');
+    clicks++;
+    console.log('broadcasting click', clicks);
+    io.emit('click-info', {clicks});
+
+    setInterval(() => {
+      socket.emit('receive-tick', {currentTime: Date.now().toString()});
+    }, 1000);
+  });
+});
 
 const Bundler = require('parcel-bundler');
 const bundler = new Bundler('./public/index.html');
 app.use(bundler.middleware());
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log('http://localhost:' + PORT);
 });
--- lab-alicia/backend/public/src/app.js
+++ lab-alicia/backend/public/src/app.js
@@ -1,11 +1,38 @@
 import React, {Component, Fragment} from 'react';
 import ReactDOM from 'react-dom';
 
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3000');
+socket.on('connect', () => {
+  console.log('client connected');
+});
+
 class App extends Component {
+  state = {currentTime: Date.now().toString()}
+
+  componentDidMount() {
+    socket.on('tick', (data) => {
+      console.log('client got tick', data);
+      this.setState({currentTime: data.currentTime})
+    })
+  }
+
+  sendClick = () => {
+    console.log('client clicked', socket.id);
+    socket.emit('send-click');
+  }
+
   render() {
     return <Fragment>
       <h1>My Socket IO App</h1>
       <p>Welcome!</p>
+      <p> 
+        <button onClick={this.sendClick}>Click here!</button>
+      </p>
+      <p>
+        The current time is: {this.state.currentTime}
+      </p>
     </Fragment>;
   }
 }
