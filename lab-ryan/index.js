c4534591310bdac077a01b70ef78bdc328be7c41
rjtj2007 <teacherman1@gmail.com>
2018-06-07 20:52:05 -0700

    nodemon bug when building app

--- lab-ryan/index.js
+++ lab-ryan/index.js
@@ -2,6 +2,20 @@ let app = require('express');
 let http = require('http').Server(app);
 let io = require('socket.io')('http');
 
+let allChats = [];
+
+io.on('connect', socket => {
+  console.log('connected', socket.id);
+  console.log('sending stuff', allChats);
+  io.emit('chat-info', {chat});
+
+  socket.on('chatting', () => {
+    console.log('got chat');
+    chat++;
+    console.log('broadcasting chats', allChats);
+    io.emit('chat-info', {allChats});
+  });
+});
 
 const Bundler = require('parcel-bundler');
 let bundler = new Bundler('./public/index.html');
